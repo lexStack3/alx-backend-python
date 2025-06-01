3ed2b40a8b6cf9d64538898a7abfe80f691aaa81
lexStack3 <alexanderedim80@gmail.com>
2025-06-01 19:20:07 +0100

    Creates a generator that streams rows from an SQL database.

---
+++ python-generators-0x00/seed.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+import mysql.connector
+from mysql.connector.connection import MySQLConnection
+from mysql.connector.connection_cext import CMySQLConnection
+from uuid import uuid4
+import csv
+
+config = {
+        'host': "localhost",
+        'user': "lex",
+        'password': "253926337"
+        }
+
+def connect_db():
+    """Connects to a MySQL Database Server."""
+    connection = mysql.connector.connect(**config)
+    return connection
+
+def create_database(connection):
+    """Creates the database `ALX_prodev` it it does not exist.
+    
+    Args:
+        connection (MySQLConnection): MySQL Database - ALX_prodev
+
+    Returns:
+        connection
+    """
+    if isinstance(connection, (MySQLConnection, CMySQLConnection)):
+        cursor = connection.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS `ALX_prodev`;")
+        connection.commit()
+        return connection
+    else:
+        return False
+
+
+def connect_to_prodev():
+    """Connects to the ALX_prodev database in MySQL."""
+    newConfig = dict(config)
+    newConfig['database'] = "ALX_prodev"
+    connection = mysql.connector.connect(**newConfig)
+    return connection
+
+def create_table(connection):
+    """Creates a table [user_data] if it does not exists.
+        - user_id(Primary Key, UUID, Indexed
+        - name (VARCHAR, NOT NULL)
+        - email (VARCHAR, NOT NULL)
+        - age (INT, NOT NULL)
+    """
+    if isinstance(connection, (MySQLConnection, CMySQLConnection)):
+        queryStr = """
+        CREATE TABLE IF NOT EXISTS `user_data` (
+        user_id CHAR(36) PRIMARY KEY,
+        name VARCHAR(256) NOT NULL,
+        email VARCHAR(256) NOT NULL,
+        age INT NOT NULL
+        ) ENGINE=InnoDB;
+        """
+        cursor = connection.cursor()
+        cursor.execute(queryStr)
+        connection.commit()
+        print("Table user_data created successfully")
+
+def insert_data(connection, data):
+    """Inserts data into the `user_data` table.
+
+    Args:
+        connection (MySQLConnection): A MySQL Database - ALX_prodev
+        data (str): Name of the csv file to copy data from.
+
+    Returns:
+        None
+    """
+    if isinstance(connection, (MySQLConnection, CMySQLConnection)):
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM user_data;")
+        old_users = cursor.fetchall()
+
+        with open(data, mode='r', newline='', encoding='utf-8') as file:
+            users = csv.DictReader(file)
+            for user in users:
+                name = user.get('name')
+                email = user.get('email')
+                age = user.get('age')
+
+                old_user = [person for person in old_users if person[1] == name and
+                            person[2] == email and int(person[3]) == age]
+                if old_user:
+                    continue
+                queryStr = """
+                INSERT INTO user_data (user_id, name, email, age)
+                VALUES (%s, %s, %s, %s);
+                """
+                cursor.execute(queryStr, (str(uuid4()), name, email, age))
+        connection.commit()
