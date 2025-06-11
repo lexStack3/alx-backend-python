7a3926f46aafd8aad03d2b4cb8be9f81613386e9
lexStack3 <alexanderedim80@gmail.com>
2025-06-11 05:29:50 +0100

    Runs multiple database queries concurrently using <asyncio.gather>

---
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Runs multiple database queries concurrently using
+<asyncio.gather>.
+"""
+import asyncio
+import aiosqlite
+
+
+async def async_fetch_users():
+    """Fetches all users from the users sqlite database."""
+    async with aiosqlite.connect('users.db') as conn:
+        async with conn.execute("SELECT * FROM users") as cursor:
+            return await cursor.fetchall()
+
+
+async def async_fetch_older_users():
+    """Fetches all users from the users.db sqlite database older
+    than the age 40.
+    """
+    async with aiosqlite.connect('users.db') as conn:
+        async with conn.execute(
+                "SELECT * FROM users WHERE id > ?", (1,)
+                ) as cursor:
+            return await cursor.fetchall()
+
+
+async def fetch_concurrently():
+    """Executes both queries concurrently."""
+    task1 = asyncio.create_task(async_fetch_users())
+    task2 = asyncio.create_task(async_fetch_older_users())
+
+    results = await asyncio.gather(task1, task2)
+
+    for users in results:
+        for user in users:
+            print(user)
+        print("{}".format('-' * 50 if users is results[:-1][0] else ''))
+
+
+if __name__ == "__main__":
+    asyncio.run(fetch_concurrently())
