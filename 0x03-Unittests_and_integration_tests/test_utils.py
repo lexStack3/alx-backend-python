49bb1cbd436d9beee506f681011057497349d81f
lexStack3 <alexanderedim80@gmail.com>
2025-06-14 15:51:48 +0100

    Tests the @memoize decorator to ensure that a method is only called once and its result is cached for subsequent accesses.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -55,6 +55,34 @@ class TestGetJson(unittest.TestCase):
 
         mock_get.assert_called_once_with(test_url)
         self.assertDictEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Unit test the utils.memoize decorator."""
+    def test_memoize(self):
+        """
+        Tests the @memoize decorator to ensure that a method is
+        only called once and its result is cached for subsequent accesses.
+
+        The test uses on the original method to verify that:
+        - The method is called during the first property access.
+        - The method is not called again on subsequent accesses.
+        - The memoized value is correctly returned both items.
+        """
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        inst = TestClass()
+        with patch.object(TestClass, 'a_method') as mock_a_meth:
+            self.assertEqual(inst.a_property, 42)
+            self.assertEqual(inst.a_property, 42)
+            mock_a_meth.assert_called_once()
 
 
 if __name__ == "__main__":
