3241c007220c07f1516ec07af5929214a0da5fc6
lexStack3 <alexanderedim80@gmail.com>
2025-06-14 15:53:12 +0100

    Tests the @memoize decorator to ensure that a method is only called once and its result is cached for subsequent accesses.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -79,7 +79,7 @@ class TestMemoize(unittest.TestCase):
                 return self.a_method()
 
         inst = TestClass()
-        with patch.object(TestClass, 'a_method') as mock_a_meth:
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_a_meth:
             self.assertEqual(inst.a_property, 42)
             self.assertEqual(inst.a_property, 42)
             mock_a_meth.assert_called_once()
