cc1550cc0a8645aedbaa975b333df84a54de7bb0
lexStack3 <alexanderedim80@gmail.com>
2025-06-14 13:48:25 +0100

    Asserts the utils.access_nested_map function

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Tests the utils.access_nested_map function."""
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test class for creating test cases on .access_nested_map.
+    """
+    @parameterized.expand([
+        ({'a': 1}, ('a',), 1),
+        ({'a': {'b': 2}}, ('a',), {'b': 2}),
+        ({'a': {'b': 2}}, ('a', 'b'), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_value):
+        """Asserts test case parameters from the parameterized.expand()
+        decorator provided.
+
+        Args:
+            nested_map (dict): A key value pair for testing.
+            path (Sequence): List of key(s) to traverse <nested_map>.
+        """
+        self.assertEqual(access_nested_map(nested_map, path=path), expected_value)
+
+
+if __name__ == "__main__":
+    unittest.main()
