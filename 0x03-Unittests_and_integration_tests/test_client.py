11e1a2d33179ed5265b53ae85ab7e00b9ca7f9ce
lexStack3 <alexanderedim80@gmail.com>
2025-06-15 12:04:14 +0100

    Tests the static method  for accurate license detection

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,13 +2,15 @@
 """Unit tests the <client> module."""
 import unittest
 from unittest.mock import patch, Mock, PropertyMock
-from parameterized import parameterized
+from parameterized import parameterized, param
 from client import GithubOrgClient
 from fixtures import TEST_PAYLOAD
+from utils import access_nested_map
 
 
 class TestGithubOrgClient(unittest.TestCase):
     """Unit tests the client.GithubOrgClient class."""
+    REPOS = [repo for repo in TEST_PAYLOAD[0][1] if repo.get('license', None)]
 
     @parameterized.expand([
         ('google', {'google_response': {'item': ['other', {}]}}),
@@ -49,8 +51,8 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('client.get_json')
     def test_public_repos(self, mock_get_json):
         """Unit tests GithubOrgClient.public_repos using a mock payload."""
-        exp_repos = [repo['name'] for repo in TEST_PAYLOAD[0][1]]
-        mock_get_json.return_value = TEST_PAYLOAD[0][1]
+        exp_repos = [repo['name'] for repo in self.REPOS]
+        mock_get_json.return_value = self.REPOS
         with patch('client.GithubOrgClient._public_repos_url',
                    new_callable=PropertyMock) as mock_pub_repos_url:
             mock_pub_repos_url.return_value = \
@@ -63,6 +65,24 @@ class TestGithubOrgClient(unittest.TestCase):
 
             mock_get_json.assert_called_once()
             mock_pub_repos_url.assert_called_once()
+
+    @parameterized.expand([
+        param(repo, access_nested_map(repo, ['license', 'key']),
+              bool(repo.get('license').get('key', None)))
+        for repo in REPOS
+    ])
+    @patch('client.get_json')
+    def test_has_license(self, load, license_key,
+                         expected_output, mock_get_json):
+        """
+        Tests the static method `has_license` for accurate
+        license detection.
+        """
+        mock_get_json.return_value = self.REPOS
+        client = GithubOrgClient('google')
+        result = client.has_license(load, license_key)
+
+        self.assertEqual(result, expected_output)
 
 
 if __name__ == "__main__":
