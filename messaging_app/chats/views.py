65ef526cb87fb246f68b875b170618d9f612934e
lexStack3 <alexanderedim80@gmail.com>
2025-11-15 21:47:51 +0100

    API endpoints for Message and Conversation

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework import permissions
+from .models import Message, Conversation
+from .serializers import MessageSerializer, ConversationSerializer
 
-# Create your views here.
+
+class ConversationViewSet(viewsets.GenericViewSet):
+    """
+    ViewSet to list conversations and create new conversations.
+    Only shows conversations the authenticated user participates in.
+    """
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    def get_queryset(self):
+        """
+        Only returns converations where the logged-in user is a participant.
+        """
+        return Conversation.objects.filter(partifipants=self.request.user)
+
+    def perform_create(self, serializer):
+        """
+        Authomatically adds the authenticated user as a participant
+        when creating a new conversation.
+        """
+        conversation = serializer.save()
+        conversation.participants.add(self.request.user)
+
+
+class MessageViewSet(viewsets.GenericViewSet):
+    """
+    ViewSet to list messages and send new messages in a conversation.
+    Users can only interact with conversations.
+    """
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+        """
+        Only returns messages from conversations the user participates in.
+        """
+        return Message.objects.filter(
+            conversation__participants=self.request.user
+        ).order_by('sent_at')
+
+    def perform_create(self, serializer):
+        conversation = serializer.validated_data['conversation']
+        
+        if not conversation.participants.filter(
+            user_id=self.request.user.user_id
+        ).exists():
+            raise PermissionDenied("You are not a participant in this conversation.")
+        serializer.save(sender=self.request.user)
