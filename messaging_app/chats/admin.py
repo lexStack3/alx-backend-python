2770abe26c701b72d44a1fad9e0065c1d5b7616e
lexStack3 <alexanderedim80@gmail.com>
2025-11-28 15:47:27 +0100

    For Django Admin Object Management

--- messaging_app/chats/admin.py
+++ messaging_app/chats/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from .models import Conversation, Message
 
-# Register your models here.
+User = get_user_model()
+
+admin.site.register(User, UserAdmin)
+
+@admin.register(Conversation)
+class ConversationApp(admin.ModelAdmin):
+    list_display = ('conversation_id', 'created_at')
+    list_filter = ('created_at',)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('message_id', 'conversation_id', 'sender_id',
+                    'message_body', 'sent_at')
+    list_filter = ('sent_at',)
