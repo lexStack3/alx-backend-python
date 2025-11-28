65008616013ef9c51616db55880df5e25319b075
lexStack3 <alexanderedim80@gmail.com>
2025-11-28 15:50:25 +0100

    Custom Authentication system

---
+++ messaging_app/chats/auth.py
@@ -0,0 +1,8 @@
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+class CustomJWTAuthentication(JWTAuthentication):
+    """
+    A Custom JWTAuthentication class.
+    """
+    pass
