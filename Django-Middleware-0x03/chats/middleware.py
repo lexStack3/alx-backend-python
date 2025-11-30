d5795ce72f0fe2718891328d178134bd2c710a35
lexStack3 <alexanderedim80@gmail.com>
2025-11-30 14:37:43 +0100

    Created a custom middleware OffensiveLanguageMiddleware for limiting the number of request per IP

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core.exceptions import PermissionDenied
-from datetime import datetime, time
+from django.http import JsonResponse, HttpResponseForbidden
+from datetime import datetime, time, timedelta
 import os
 
 
@@ -44,3 +45,53 @@ class RestrictAccessByTimeMiddleware:
                 )
 
         return self.get_response(request)
+
+
+class OffensiveLanguageMiddleware:
+    """
+    Limits the number of chat messages a user can send within a certain time
+    window, based on their IP address.
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.time_window = timedelta(minutes=1)
+        self.max_requests = 5
+        self.ip_request_log = {}
+
+    def __call__(self, request):
+        if request.method == "POST":
+            ip = self.get_client_ip(request)
+            now = datetime.now()
+
+            if ip not in self.ip_request_log:
+                self.ip_request_log[ip] = []
+
+            timestamps = [
+                t for t in self.ip_request_log[ip]
+                if now - t < self.time_window
+            ]
+
+            if len(timestamps) >= self.max_requests:
+                accept = request.headers.get("Accept", "")
+                if "application/json" in accept:
+                    return JsonResponse(
+                        {"error": "Rate limit exceeded. Try again later."},
+                        status=429
+                    )
+                return HttpResponseForbidden(
+                    "<h3>429 Too Many Requests</h3><p>Please wait a minute.</p>"
+                )
+
+            timestamps.append(now)
+            self.ip_request_log[ip] = timestamps
+
+        return self.get_response(request)
+
+    def get_client_ip(self, request):
+        """
+        Returns a user's IP address
+        """
+        x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+        if x_forwarded_for:
+            return x_forwarded_for.split(',')[0]
+        return request.META.get("REMOTE_ADDR")
