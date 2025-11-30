ddcf941fcddc6a198c8a58bb824fbf1af8d09ae5
lexStack3 <alexanderedim80@gmail.com>
2025-11-30 08:17:36 +0100

    Added the custom middleware for timeframe restriction

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'chats.middleware.RestrictAccessByTimeMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
