d108ec9d59106978051dda83a9a16a7a3fdd1399
lexStack3 <alexanderedim80@gmail.com>
2025-11-30 20:27:25 +0100

    Setup for admin page

---
+++ Django-signals_orm-0x04/messaging/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from .models import Message, Notification
+
+User = get_user_model()
+
+admin.site.register(User, UserAdmin)
+
+@admin.register(Notification)
+class NotificationApp(admin.ModelAdmin):
+    list_display = ('notification_id',
+                    'sender_id', 'message_id',
+                    'read', 'created_at')
+    list_filter = ('created_at',)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('message_id', 'sender_id',
+                    'receiver_id', 'content',
+                    'created_at')
+    list_filter = ('created_at',)
