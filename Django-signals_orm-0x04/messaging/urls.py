c6a5c461a0f3fbdf3d9187832ba07a8f7d70b193
lexStack3 <alexanderedim80@gmail.com>
2025-11-30 22:38:33 +0100

    Added endpoint for delete a user

--- Django-signals_orm-0x04/messaging/urls.py
+++ Django-signals_orm-0x04/messaging/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('delete-user/<uuid:user_id>/', views.delete_user,
+         name='delete-user'),
+    path('list-users/', views.list_users,
+         name='list-users'),
 ]
