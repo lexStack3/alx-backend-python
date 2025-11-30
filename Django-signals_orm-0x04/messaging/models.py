9e9611a93811cf18a4ac28c4b3782ffb15941915
lexStack3 <alexanderedim80@gmail.com>
2025-11-30 20:25:25 +0100

    Created a custom User model, Message, and Notification model.

---
+++ Django-signals_orm-0x04/messaging/models.py
@@ -0,0 +1,76 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    """
+    Model representation of a User instance.
+    """
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                               editable=False)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = [
+        "username",
+        "first_name",
+        "last_name"
+    ]
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["email"]),
+        ]
+
+    def __str__(self):
+        """
+        String representation of a <User> instance.
+        """
+        return self.username
+
+
+class Message(models.Model):
+    """
+    Model representation of a Message instance.
+    """
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                                  editable=False)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="sent_messages")
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE,
+                                 related_name="received_messages")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        """
+        String representation of a <Message> instance.
+        """
+        return "Message {}: {} to {}".format(
+            self.message_id,
+            self.sender.username,
+            self.receiver.username
+        )
+
+
+class Notification(models.Model):
+    """
+    Model representation of a <Notification> instance.
+    """
+    notification_id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                                        editable=False)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """
+        String representation of a <Notification> instance.
+        """
+        return "Notif from {}: {}".format(
+            self.sender.username,
+            self.message.content[:20]
+        )
