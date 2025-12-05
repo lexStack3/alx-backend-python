94a8ee2decf5b26aff355356429462c6e3c40b5b
lexStack3 <alexanderedim80@gmail.com>
2025-12-05 14:33:13 +0100

    Configured CACHE system

--- Django-signals_orm-0x04/messaging/settings.py
+++ Django-signals_orm-0x04/messaging/settings.py
@@ -144,3 +144,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'messaging.User'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique.snowflake'
+    }
+}
