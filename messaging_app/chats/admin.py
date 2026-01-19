3cc1b45982dbef8719c2a623b6fc08d902450841
lexStack3 <alexanderedim80@gmail.com>
2026-01-19 06:39:52 +0100

    Configured the UserAdmin view

--- messaging_app/chats/admin.py
+++ messaging_app/chats/admin.py
@@ -5,7 +5,23 @@ from .models import Conversation, Message
 
 User = get_user_model()
 
-admin.site.register(User, UserAdmin)
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2')
+        }),
+    )
 
 @admin.register(Conversation)
 class ConversationApp(admin.ModelAdmin):
